3f3f54f19dc808b1f0419fe30a5bceec54fe1356
rahulmandal1994 <mandalrahul4@gmail.com>
2022-05-12 13:34:26 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-ivy-qf2sbt
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-ivy-qf2sbt)
---
+++ src/app/app.component.ts
@@ -0,0 +1,24 @@
+import { Component, VERSION } from '@angular/core';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  name = 'Angular ' + VERSION.major;
+  ShowedIndex:number;
+  array:any=[
+    {text:'Home'},
+    {text:'Home'},
+    {text:'Pictures'},
+    {text:'Summer'},
+    {text:'Summer 15'},
+    {text:'Italy'},
+  ]
+  ngOnInit(){
+    //console.log("--LENTH--",ShowedIndex-3)
+    this.ShowedIndex=this.array.length-3;
+  }
+
+}
